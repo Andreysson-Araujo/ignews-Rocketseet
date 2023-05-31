95cb4d8b835d18cd091938fb7cadd143932ecf94
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-05-31 12:28:14 -0500

    Integração com API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,60 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
-
-import styles from "./home.module.scss"
+import styles from "./home.module.scss";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "@/services/stripe";
 
 
-export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Home | ig.news</title>
-            </Head>
-            <main className={styles.contentContainer}>
-                <section className={styles.hero}>
-                    <span>👏 Hey, welcome</span>
-                    <h1>News aboute the <span>React</span> world</h1>
 
-                    <p>
-                        Get acesss to all the publications <br />
-                        <span>for $9.90 </span>
-                    </p>
-                    <SubscribeButton />
-                </section>
-
-                <img src="/img/avatar.svg" alt="Girl coding" />
-            </main>
-        </>
-    )
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  }
 }
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News aboute the <span>React</span> world
+          </h1>
+
+          <p>
+            Get acesss to all the publications <br />
+            <span>for {product.amount} mounth </span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/img/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
+
+export const getServerSideProps: GetStaticProps<HomeProps> = async () => {
+  const price = await stripe.prices.retrieve("price_1NDr8MEMshT8GRwqlVm4EW8y");
+
+  const product = {
+    priceId: price.id,
+    amount:price.unit_amount ? new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    }).format(price.unit_amount / 100) : "",
+  };
+
+  return {
+    props: {
+      product,
+      revalidate: 60 * 60 * 24 //24HORAS
+    },
+  };
+};
---
+++ src/services/stripe.ts
@@ -0,0 +1,16 @@
+import Stripe from "stripe";
+import { version } from "../../package.json";
+
+const stripeApiKey = process.env.STRIPE_API_KEY;
+
+if (!stripeApiKey) {
+  throw new Error("Stripe API key not found in environment variables.");
+}
+
+export const stripe = new Stripe(stripeApiKey, {
+  apiVersion: "2022-11-15",
+  appInfo: {
+    name: "Ignews",
+    version,
+  },
+});
